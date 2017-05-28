04b7a04769cda2e4c528a400ce8fce5867e28cf0
Bettadapura S. Sharma <bsrsharma@yahoo.com>
2017-05-27 18:14:28 -0700

    Add files via upload

--- Anonymizer.java
+++ Anonymizer.java
@@ -29,64 +29,40 @@ public final class Anonymizer
 {
    // replace quote with space and embedded comma with underscore
    // as these will cause errors in converting a .csv to RDD
-   public static String unquoteRemoveEmbeddedCommas (String s)
+   static class unquoteRemoveEmbeddedCommas implements Function<String, String>   
    {
-      boolean quoteSeen  = false;
-      char[] t = new char[s.length()];
-      int i;
-	 
-      for (i = 0; i < s.length(); i++)
+      @Override
+      public String call(String s)
       {
-	 t[i] = s.charAt(i);
-         if ((s.charAt(i) == '\'' ) || (s.charAt(i) == '"'))
+         boolean quoteSeen  = false;
+         char[] t = new char[s.length()];
+         int i;
+	 
+         for (i = 0; i < s.length(); i++)
          {
-	    if (quoteSeen == true)
+	    t[i] = s.charAt(i);
+            if ((s.charAt(i) == '\'' ) || (s.charAt(i) == '"'))
             {
-                quoteSeen = false;
+	       if (quoteSeen == true)
+               {
+                  quoteSeen = false;
+               }
+               else
+               {
+	          quoteSeen = true;
+               }			
+               t[i] = ' ';
             }
-            else
+            if ((s.charAt(i) == ',') && (quoteSeen == true))
             {
-	        quoteSeen = true;
-            }			
-            t[i] = ' ';
+	       t[i] = '_';
+            }         	  
          }
-         if ((s.charAt(i) == ',') && (quoteSeen == true))
-         {
-	    t[i] = '_';
-         }         	  
-      }
 	 
 	 return String.valueOf(t);
-   }  // unquoteremoveEmbeddedCommas
-  
-
-   public static String makeNiceStr (byte[] AoB) 
-   {
-      String niceAoC = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
-
-      char[] AoC = new char[AoB.length];
-      int i;
-      int myInt;
-      char c;
-      
+      }	 
+   }  // unquoteRemoveEmbeddedCommas
    
-      for (i = 0; i < AoB.length; i++)
-      {
-         // AoB[i] = AoB[i] & 31
-         myInt = AoB[i];
-         myInt = myInt & 31;
-         // prevent first char numeric (to prevent all numeric labels!)	  
-	 if ((i == 0) && (myInt > 23))
-	 {
-	    myInt = myInt - 24;
-         }
-         c = niceAoC.charAt(myInt);
-	 AoC[i] = c;
-      }
-      return(String.valueOf(AoC));
-    }
-    
-    
 // This function takes a string and generates a similar looking anonymized string
    public static String makeAnonStr(String Str) 
    {
@@ -136,18 +112,6 @@ public final class Anonymizer
    }
     
     
-   public static void shiftAndScaleMat (int maxRow, int maxCol, double mean[], double stdDev[], DenseMatrix origData, double arrn[][] )
-   {
-   
-      int i,j;
-
-      for (j = 0; j < maxCol; j++)
-         for (i = 0; i < maxRow; i++)
-	 {
-	    arrn[i][j] = mean[j] + stdDev[j]*origData.apply(i, j);
-         }		  
-   }
-   
    static class ParseDouble implements Function<String[], double[]>
    {
       @Override
@@ -173,6 +137,7 @@ public final class Anonymizer
    static double mean[], stdDev[];
    static String typeStrings[];
    static Random rng;
+   static int maxCount = 0;
    
    static class Anon implements Function<String[], String[]>
    {
@@ -209,7 +174,7 @@ public final class Anonymizer
       MultivariateStatisticalSummary summary;
       
       JavaRDD<String> inpFile = sc.textFile(inputFileName);
-
+      
       numRows = (int)inpFile.count();
 
       System.out.println("Read " + inputFileName + ", Read " + numRows + " Rows");
@@ -217,9 +182,70 @@ public final class Anonymizer
       numCols = inpFile.first().split(",").length;
       
       System.out.println("print out a few rows read from file");
-      System.out.println( inpFile.take(5));
+      System.out.println( inpFile.take(5));      
       
-      JavaRDD<Vector> parsedInpV = inpFile.map(s -> s.split(",")).map(new ParseDouble()).map(dA -> Vectors.dense(dA));
+      // filter element to remove quotes to prevent (quote) embedded commas
+      JavaRDD<String> inpFileUq = inpFile.map(new unquoteRemoveEmbeddedCommas());
+      // System.out.println( inFileUq.take(5));
+      
+      Integer countFields[] = new Integer[numRows];
+      countFields = inpFileUq.map(s -> s.split(",").length).collect().toArray(countFields);
+      
+      boolean RectangularizationNeeded = false;
+  
+      System.out.println("Compute number of fields in each row: ");
+
+      int maxCountAt = 0;
+  
+      for (i = 0; i < numRows; i++)
+      {
+         if (countFields[i] > maxCount)
+	 {
+	    maxCount = countFields[i]; maxCountAt = i;
+         }
+	
+         if ((RectangularizationNeeded == false) && (i > 0))
+	 {
+	    if (countFields[i] != countFields[i-1])
+	    {
+	       RectangularizationNeeded = true;
+	    }
+         }	   
+      }	
+  
+      if (RectangularizationNeeded == true)
+	 System.out.println("Identified jagged data set; Rectangularization needed");
+      else
+	 System.out.println("Identified rectangular data set; No fixup needed");
+
+      System.out.println("Inferring longest row(s) has " + maxCount + " fields at row " + maxCountAt);
+  
+      // rectangularize the RDD to numRows X maxCount
+      
+      String header = inpFileUq.first();
+      System.out.println("header: " + header);
+
+      JavaRDD<String> inpFileNh = inpFileUq.filter(row -> (row.equals(header) == false));
+  
+      System.out.println("Removed the First row as Header");
+      
+
+      JavaRDD<String> inpFileRe = inpFileNh.map(s -> s + ",No Data");
+      // remove short rows
+      JavaRDD<String> shortFile = inpFileRe.filter(row -> row.split(",").length < maxCount+1);
+      System.out.println("Short rows will be filtered out");
+      System.out.println(shortFile.take(40));		  
+      // truncate to maxCount+1 columns
+      JavaRDD<String> inpFileTr = inpFileRe.filter(row -> row.split(",").length == maxCount + 1);
+      numRows = (int)inpFileTr.count();
+      System.out.println("number of rows = " + numRows);
+     
+      numCols = inpFileTr.first().split(",").length;
+      
+      System.out.println("print out a few rows");
+      System.out.println( inpFileTr.take(5));
+      
+      JavaRDD<Vector> parsedInpV = inpFileTr.map(s -> s.split(",")).map(new ParseDouble()).map(dA -> Vectors.dense(dA));
       
       summary = Statistics.colStats(parsedInpV.rdd());
       
@@ -234,10 +260,8 @@ public final class Anonymizer
       System.out.println("summary.normL2"); System.out.println(summary.normL2());
       System.out.println("summary.numnonZeros"); System.out.println(summary.numNonzeros()); 
       
-      //double mean[] = new double[numCols];
       mean = new double[numCols];
       double variance[] = new double[numCols];
-      //double stdDev[] = new double[numCols];
       stdDev = new double[numCols];
       
       for (j = 0; j < numCols; j++)
@@ -247,7 +271,6 @@ public final class Anonymizer
          stdDev[j] = Math.sqrt(variance[j]);         
       }
       
-      //String typeStrings[] = new String[numCols];
       typeStrings = new String[numCols];
       
       // infer columns where normL1, normL2, mean, variance, max and mean are 0 as non-numeric
@@ -279,10 +302,9 @@ public final class Anonymizer
          System.out.print( s + "\t" );
       System.out.println();
       
-      //Random rng = new Random();
       rng = new Random();
       
-      JavaRDD<String[]> outData = inpFile.map(s -> s.split(",")).map(new Anon());
+      JavaRDD<String[]> outData = inpFileTr.map(s -> s.split(",")).map(new Anon());
       
       System.out.println("outData:");
       for (String[] sA: outData.take(5))
@@ -290,102 +312,15 @@ public final class Anonymizer
          for (String s: sA)
             System.out.print( s + "\t" );
          System.out.println();
-      }       
-      
-/*******************************************************************************      
-      // double arrn[][] = new double[numRows][numCols];
-      List<String> AlistOfStr= new ArrayList<String>();
-      
-      AlistOfStr = inpFile.collect();
-      System.out.println(AlistOfStr.get(0) + "....." + AlistOfStr.get(numRows-1));
-      
-      //System.out.println("DenseMatrix randn( " + numRows + " , " + numCols + " )");
-
-      //double values[] = new double[numRows*numCols];
-      
-      //DenseMatrix origData = new DenseMatrix(numRows, numCols, values);
-      //origData = origData.randn(numRows, numCols, rng);
-
-      System.out.println("print out a few rows generated....");
-
-      String[] aOfStr  = new String[numCols];
-      
-      for (i = 0; i < Math.min(4, numRows); i++)
-      {
-         aOfStr = AlistOfStr.get(i).split(",");
-         for (j = 0; j < numCols; j++)
-         {
-            if (typeStrings[j] == "String")
-               System.out.print(makeAnonStr(aOfStr[j]) + "\t");
-            else
-               System.out.print(rng.nextGaussian() + "\t");
-         }
-         System.out.println();
       }
-	  
-      System.out.println("using means and variances of:");
-
-      System.out.println("means:");
-      for (j = 0; j < numCols; j++)
-      {
-         System.out.print(mean[j] + " , ");
-      }
-      System.out.println();
-
-      System.out.println("variances:");
-      for (j = 0; j < numCols; j++)
-      {
-         System.out.print(variance[j] + " , ");
-      }
-      System.out.println();
-
-      //shiftAndScaleMat(numRows, numCols, mean, stdDev, origData, arrn);
-
-      System.out.println("print out a few rows of transformed data" );
-
-      for (i = 0; i < Math.min(4, numRows); i++)
-      {
-         aOfStr = AlistOfStr.get(i).split(",");      
-         for (j = 0; j < numCols; j++)
-         {
-            if (typeStrings[j] == "String")
-               System.out.print(makeAnonStr(aOfStr[j]) + "\t");
-            else         
-               System.out.print((rng.nextGaussian()*stdDev[j] + mean[j]) + "\t");
-         }
-         System.out.println();
-      }
-
+      
+      numCols--; // remove "No Data"
+      
       try
       {
          PrintWriter fileHandle = new PrintWriter(outputFileName);
-
-         for (i = 0; i < numRows; i++)
-         {
-            aOfStr = AlistOfStr.get(i).split(",");      
-            if (numCols > 1)
-            {
-               for (j = 0; j < numCols - 1; j++)
-               {
-                  if (typeStrings[j] == "String")
-                     fileHandle.print(makeAnonStr(aOfStr[j]));
-                  else                           
-                     fileHandle.print(String.valueOf(rng.nextGaussian()*stdDev[j] + mean[j]));
-                  fileHandle.print(",");
-               }
-            }
-            if (typeStrings[j] == "String")
-               fileHandle.println(makeAnonStr(aOfStr[numCols - 1]));
-            else                           
-               fileHandle.println(String.valueOf(rng.nextGaussian()*stdDev[numCols - 1] + mean[numCols - 1]));            
-         }
-         fileHandle.close();
-      }
-*******************************************************************************/
-
-      try
-      {
-         PrintWriter fileHandle = new PrintWriter(outputFileName);
+         
+         fileHandle.println(header);
          
          for (String[] sA: outData.take(numRows))
          {
@@ -397,7 +332,7 @@ public final class Anonymizer
                   fileHandle.print(s);
                   fileHandle.print(",");
                }
-               else
+               if (j == numCols - 1)
                   fileHandle.println(s);
                j++;
             }
@@ -422,31 +357,9 @@ public final class Anonymizer
       JavaRDD<String[]> parsedData = arrnFile.map(s -> s.split(","));
       
       System.out.println("print out a few vectors after converting from strings");
-/*      
-      for (String[] sA: parsedData.take(5))
-      {
-         for (String s: sA)
-            System.out.print( s + "\t" );
-         System.out.println();
-      }             
-*/      
+
       JavaRDD<double[]> parsedDataD = parsedData.map(new ParseDouble());
-/*      
-      for (double[] dA: parsedDataD.take(5))
-      {
-         for (double d: dA)
-            System.out.print( d + "\t" );
-         System.out.println();
-      }
-      
-      i = 0;
-      for (double[] dA: parsedDataD.take(numRows))
-      {
-         if ((dA[0] != 0.0) || (dA[2] != 0.0) || (dA[4] != 0.0))
-           System.out.println(i+"\t"+dA[0]+"\t"+dA[1]+"\t"+dA[2]+"\t"+dA[3]+"\t"+dA[4]+"\t"+dA[5]);
-         i++;           
-      }
-*/      
+
       JavaRDD<Vector> parsedDataV = parsedDataD.map(dA -> Vectors.dense(dA));
       
       System.out.println( parsedDataV.take(5) );
